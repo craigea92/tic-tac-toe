ac0f7ccde8cef7310404fe1f64430e7881158c59
Craig Eaton <craigea92@gmail.com>
2024-04-02 15:57:55 +0100

    created a basic grid numbered from 1 to 9

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿string[] grid = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+for (int i = 0; i < 3; i++) {
+  for (int j = 0; j < 3; j++) {
+    Console.Write(grid[i * 3 + j] + "|");
+  }
+  Console.WriteLine();
+  Console.WriteLine("------");
+}
