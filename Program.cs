e586b78a8b7cab8ee95af3f3dfd71e9e80ac2812
Craig Eaton <craigea92@gmail.com>
2024-04-02 16:11:40 +0100

    Added turn based logic and terminal input for X and O

--- Program.cs
+++ Program.cs
@@ -1,5 +1,27 @@
 ﻿string[] grid = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-PrintGrid();
+bool isPlayer1Turn = true;
+
+while (true) {
+  PrintGrid();
+
+  if (isPlayer1Turn)
+    Console.WriteLine("Player 1 turn!");
+    else
+    Console.WriteLine("Player 2 turn!");
+  
+  string choice = Console.ReadLine();
+
+  if(grid.Contains(choice) && choice != "X" && choice !="O") {
+    int gridIndex = Convert.ToInt32(choice) -1;
+
+    if (isPlayer1Turn)
+      grid[gridIndex] = "X";
+    else
+      grid[gridIndex] ="O";
+  }
+
+  isPlayer1Turn = !isPlayer1Turn;
+}
 
 // Void is an effective tool, it enables us to design a method to carry out operations without returning data
 void PrintGrid(){
--- obj/Debug/net8.0/tictactoe.AssemblyInfo.cs
+++ obj/Debug/net8.0/tictactoe.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tictactoe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac0f7ccde8cef7310404fe1f64430e7881158c59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+415d25a29e50f3fb424a1b55a2a287b1e906dda3")]
 [assembly: System.Reflection.AssemblyProductAttribute("tictactoe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tictactoe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
